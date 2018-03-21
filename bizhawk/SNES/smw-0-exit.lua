ef6931d067709ca83493f6c1bbc1e5517f610863
Hazel Virdó <github@virdo.name>
2018-03-21 00:47:15 -0400

    fix display

--- bizhawk/SNES/smw-0-exit.lua
+++ bizhawk/SNES/smw-0-exit.lua
@@ -3,13 +3,16 @@ SUCCESS_COLOR = "lightgreen"
 TEXT_COLOR = "white"
 
 LINE_HEIGHT = 12
+DESC_COLUMN = 0
+VALUE_COLUMN = 175
+MARIO_COLUMN = 325
 
 MARIO_X_REGISTER = 0x0094 -- x coord, absolute (0x007E is screen-relative)
 MARIO_LEFT_OFFSET = 238 -- mario x for shell 3 (384); 0x92 = 146
 MARIO_RIGHT_OFFSET = MARIO_LEFT_OFFSET - 36
 
--- sprite slot constants
-SPRITE_SLOT = {
+-- sprite slot constants using dots video reference values
+SPRITE_SLOTS = {
     [0] = {
         ["register"] = 0x00E4,
         ["correct_value"] = 0xA9,
@@ -20,81 +23,68 @@ SPRITE_SLOT = {
         ["register"] = 0x00E5,
         ["correct_value"] = 0x1C,
         ["mario_direction"] = 0,
-        ["desc"] = "red shell"
+        ["desc"] = "koopa 1"
     },
     [2] = {
         ["register"] = 0x00E6,
         ["correct_value"] = 0x92,
         ["mario_direction"] = 1,
-        ["desc"] = "red shell"
+        ["desc"] = "koopa 2"
     },
     [3] = {
         ["register"] = 0x00E7,
         ["correct_value"] = 0x75,
         ["mario_direction"] = 1,
-        ["desc"] = "red shell"
+        ["desc"] = "koopa 3"
     },
     [4] = {
         ["register"] = 0x00E8,
         ["correct_value"] = 0x4C,
         ["mario_direction"] = 0,
-        ["desc"] = "red shell"
+        ["desc"] = "koopa 4"
     },
     [5] = {
         ["register"] = 0x00E9,
         ["correct_value"] = 0x46,
         ["mario_direction"] = 0,
-        ["desc"] = "red shell"
+        ["desc"] = "koopa 5"
     },
     [6] = {
         ["register"] = 0x00EA,
-        ["correct_value"] = 0x0,
-        ["mario_direction"] = -1,
-        ["desc"] = "? red shell"
-    },
-    [7] = { -- TODO
-        ["register"] = 0x0,
-        ["correct_value"] = 0x0,
-        ["mario_direction"] = -1,
+        ["correct_value"] = 0xFF,
+        ["mario_direction"] = -1, -- TODO: no direction necessary?
         ["desc"] = "p switch"
-    },
-    [9] = { -- TODO
-        ["register"] = 0x0,
-        ["correct_value"] = 0x0,
-        ["mario_direction"] = -1,
-        ["desc"] = "glitched berry"
-    },
-    [10] = { -- TODO
-        ["register"] = 0x0,
-        ["correct_value"] = 0x0,
-        ["mario_direction"] = -1,
-        ["desc"] = "glitched berry"
     }
 }
 
 while true do
     local mario_x = mainmemory.read_u16_le(MARIO_X_REGISTER) 
-    gui.text(0, LINE_HEIGHT * 8, "mario: " .. mario_x, "white")
+    gui.text(DESC_COLUMN, 0, "sprite slot", "white")
+    gui.text(VALUE_COLUMN, 0, "0xHEX (delta)", "white")
+    gui.text(MARIO_COLUMN, 0, "<- mario ->", "white")
 
-    for i=0,6 do
-        ss = SPRITE_SLOT[i]
+    for i=0,table.getn(SPRITE_SLOTS) do
+        ss = SPRITE_SLOTS[i]
         local ss_value = mainmemory.read_u8(ss.register)
         local ss_delta = ss_value - ss.correct_value
-
         local mario_offset = ss.mario_direction == 0 and MARIO_LEFT_OFFSET or MARIO_RIGHT_OFFSET
         local mario_delta = mario_x - ss.correct_value - mario_offset
+        
+        local display_height = LINE_HEIGHT * (i+1)
+        gui.text(DESC_COLUMN, display_height, i .. ": " .. ss.desc, "yellow")
 
-        local display_height = LINE_HEIGHT * i
-        local display_text = "[ss" .. i .. ", " .. ss.desc .. "] " .. ss_value
+        local display_value = "0x" .. string.format("%02X", ss_value)
         if ss_delta == 0 then -- register is correctly set
-            gui.text(0, display_height, display_text, SUCCESS_COLOR)
+            gui.text(VALUE_COLUMN, display_height, display_value, SUCCESS_COLOR)
         else
-            gui.text(0, display_height, display_text .. " (" .. ss_delta .. ")", FAIL_COLOR)
-            if mario_delta == 0 then
-                gui.text(300, display_height, "there!", SUCCESS_COLOR)
+            gui.text(VALUE_COLUMN, display_height, display_value .. "  (" .. ss_delta .. ")", FAIL_COLOR)
+            if mario_delta == 0 then -- mario is in the right spot
+                gui.text(MARIO_COLUMN, display_height, "  there!", SUCCESS_COLOR)
             else
-                local nudge = mario_delta < 0 and (math.abs(mario_delta) .. " ->") or ("<- " .. math.abs(mario_delta))
-                gui.text(300, display_height, nudge, FAIL_COLOR)
+                local right_arrow_spaces = string.rep(" ", 4 - math.floor(math.log10(math.abs(mario_delta))))
+                local go_right_string = "    " .. math.abs(mario_delta) .. right_arrow_spaces .. "->"
+                local go_left_string = "<-  " .. math.abs(mario_delta)
+                gui.text(MARIO_COLUMN, display_height, mario_delta < 0 and go_right_string or go_left_string, FAIL_COLOR)
             end
         end
     end
